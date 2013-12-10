596c7fe9ad37bdfe503f149cd8cf662e75564db2
Sebastien Beau <sebastien.beau@akretion.com>
2013-12-10 17:44:32 +0100

    [IMP] improve timesheet, add onchange (/home/sebastien/DEV/openerp/70/service/src-invoice-seb/project_commercial_management rev 19.1.1)

--- project_invoicing/project.py
+++ project_invoicing/project.py
@@ -237,7 +237,34 @@ class HrAnalyticTimesheet(orm.Model):
         #MAYBE REPLACE BY SUBCONTRACTOR
         'invoice_line_id': fields.many2one('account.invoice.line', 'Invoice Line'),
     }
+    
+    def on_change_account_id(self, cr, uid, ids, account_id, user_id=False, context=None):
+        account_obj = self.pool.get('account.analytic.account')
+        res = super(HrAnalyticTimesheet, self).\
+                on_change_account_id(cr, uid, ids, account_id, user_id)
+        
+        if not res.get('domain'):
+            res['domain'] = {}
+         
+        res['domain']['task_id'] = [
+            ('state', '=', 'open'),
+            ('analytic_account_id', '=', account_id)
+        ]
+        return res
 
+    def on_change_task_id(self, cr, uid, ids, task_id, context=None):
+        res = {}
+        res['value'] = {}
+        task_obj=self.pool.get('project.task')
+        task=task_obj.browse(cr, uid, task_id, context=context)
+        if task.fixed_amount:
+            res['value']['to_invoice'] = 5
+        else:
+            res['value']['to_invoice'] = 1
+        return res
+
+
+    #TODO FIXME still usefull?
     def on_change_unit_amount(self, cr, uid, sheet_id, prod_id, unit_amount,
                               company_id, unit=False, journal_id=False,
                               task_id=False, to_invoice=False, context=None):
